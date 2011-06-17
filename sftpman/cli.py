c43cc91294541d95a7342626f0d545643105b8d6
Slavi Pantaleev <s.pantaleev@gmail.com>
2011-06-17 19:43:56 +0300

    Fixed CLI to work with the new models code.

---
+++ sftpman/cli.py
@@ -0,0 +1,110 @@
+import sys
+
+from exception import SftpException
+from model import EnvironmentModel, SystemModel, SystemControllerModel
+
+
+class SftpCli(object):
+
+    def __init__(self):
+        self.environment = EnvironmentModel()
+
+    def command_help(self, *args, **kwargs):
+        """Displays this help menu."""
+        print("Commands available:\n")
+        for name in dir(self):
+            if not name.startswith("command_"):
+                continue
+            name_clean = name[len("command_"):]
+            print("%s:\n - %s\n" % (name_clean, getattr(self, name).__doc__))
+
+    def command_ls(self, list_what):
+        """Lists the available/mounted/unmounted sftp systems.
+
+        Usage: sftpman ls {what}
+        Where {what} is one of: available, mounted, unmounted
+        """
+        if list_what in ('available', 'mounted', 'unmounted'):
+            callback = getattr(self.environment, 'get_%s_ids' % list_what)
+            lst = callback()
+        else:
+            lst = []
+        if len(lst) != 0:
+            print("\n".join(lst))
+
+    def command_mount(self, system_id):
+        """Mounts the specified sftp system, unless it's already mounted.
+
+        Usage: sftpman mount {id}
+        """
+        try:
+            system = SystemModel.create_by_id(system_id, self.environment)
+            controller = SystemControllerModel(system, self.environment)
+            controller.mount()
+        except SftpException, e:
+            sys.stderr.write('Cannot mount: %s' % str(e))
+            # @todo - fix exit status number
+            sys.exit(1)
+
+    def command_unmount(self, system_id):
+        """Unmounts the specified sftp system.
+
+        Usage: sftpman unmount {id}
+        """
+        try:
+            system = SystemModel.create_by_id(system_id, self.environment)
+            controller = SystemControllerModel(system, self.environment)
+            controller.unmount()
+        except SftpException, e:
+            sys.stderr.write('Cannot unmount: %s' % str(e))
+            # @todo - fix exit status number
+            sys.exit(1)
+
+    def command_mount_all(self):
+        """Mounts all sftp file systems known to sftpman.
+
+        Usage: sftpman mount_all
+        """
+        has_failed = False
+        for system_id in self.environment.get_unmounted_ids():
+            try:
+                system = SystemModel.create_by_id(system_id, self.environment)
+                controller = SystemControllerModel(system, self.environment)
+                controller.mount()
+            except SftpException, e:
+                sys.stderr.write('Cannot mount: %s' % str(e))
+                has_failed = True
+        # @todo - fix exit status number
+        sys.exit(0 if not has_failed else 1)
+
+    def command_unmount_all(self):
+        """Unmounts all sftp file systems known to sftpman.
+
+        Usage: sftpman unmount_all
+        """
+        has_failed = False
+        for system_id in self.environment.get_mounted_ids():
+            try:
+                system = SystemModel.create_by_id(system_id, self.environment)
+                controller = SystemControllerModel(system, self.environment)
+                controller.unmount()
+            except SftpException, e:
+                sys.stderr.write('Cannot unmount: %s' % str(e))
+                has_failed = True
+        # @todo - fix exit status number
+        sys.exit(0 if not has_failed else 1)
+
+
+def start():
+    command = sys.argv[1]
+    args = sys.argv[2:]
+
+    instance = SftpCli()
+    callback = getattr(instance, "command_%s" % command, None)
+    if callable(callback):
+        try:
+            callback(*args)
+        except TypeError, e:
+            sys.stderr.write('Bad call for %s: %s' % (command, str(e)))
+    else:
+        instance.command_help()
