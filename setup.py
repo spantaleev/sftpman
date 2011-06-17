85c3cd89acc2e341aa598df4a1e70fd3e0b4a8be
Slavi Pantaleev <s.pantaleev@gmail.com>
2011-06-17 20:07:30 +0300

    Fixed setup script.

--- setup.py
+++ setup.py
@@ -2,22 +2,22 @@
 SftpMan
 =======
 
-SftpMan is a command-line and GUI application that makes it easy to setup and mount SSHFS/SFTP file systems.
+SftpMan is a command-line application (with a GUI frontend packaged separately) that makes it easy to setup and mount SSHFS/SFTP file systems.
 
-It allows you to setup all your SFTP systems using a GUI and easily mount/unmount them (from the GUI or the CLI).
+It allows you to define all your SFTP systems and easily mount/unmount them.
 """
 
 from setuptools import setup
 
 setup(
     name = "SftpMan",
-    version = '0.1.3',
-    description = "A command-line and GTK application that helps you mount SFTP file systems.",
+    version = '0.2.0',
+    description = "A command-line application that helps you mount SFTP file systems.",
     long_description = __doc__,
     author = "Slavi Pantaleev",
     author_email = "s.pantaleev@gmail.com",
     url = "https://github.com/spantaleev/SftpMan",
-    keywords = ["sftp", "ssh", "sshfs", "gtk"],
+    keywords = ["sftp", "ssh", "sshfs"],
     license = "BSD",
     packages = ['sftpman'],
     entry_points="""
@@ -29,14 +29,11 @@ setup(
         "Programming Language :: Python",
         "Development Status :: 4 - Beta",
         "Environment :: Console",
-        "Environment :: X11 Applications :: GTK",
         "Intended Audience :: Developers",
         "Intended Audience :: System Administrators",
         "License :: OSI Approved :: BSD License",
         "Operating System :: Unix",
         "Topic :: Communications :: File Sharing",
-        "Topic :: Desktop Environment :: File Managers",
-        "Topic :: Desktop Environment :: Gnome",
         "Topic :: Internet",
         "Topic :: Internet :: File Transfer Protocol (FTP)",
         "Topic :: System :: Networking",
