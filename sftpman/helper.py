2dee7df90961faa39ab6958f81ff5fd5445bab98
Slavi Pantaleev <s.pantaleev@gmail.com>
2011-06-17 22:17:45 +0300

    Helper cleanups.

--- sftpman/helper.py
+++ sftpman/helper.py
@@ -22,9 +22,9 @@ if json is None:
             raise RuntimeError('You need a JSON library to use sftpman!')
     json = _JSON()
 
+
 def mkdir_p(path):
     """NOOP if the directory exists. If not, it creates the the whole directory tree."""
-
     try:
         os.makedirs(path)
     except OSError as exc:
@@ -34,7 +34,6 @@ def mkdir_p(path):
 
 def rmdir(path):
     """Safe rmdir (non-recursive) which doesn't throw if the directory is not empty."""
-
     try:
         os.rmdir(path)
     except OSError as exc:
@@ -43,11 +42,9 @@ def rmdir(path):
 
 def shell_exec(command):
     """Executes the given shell command and returns its output."""
-
     return subprocess.Popen(command, shell=True, stdout=subprocess.PIPE).communicate()[0]
 
 
 def kill_pid(pid, signal):
     """Sends a signal to the process with the given id."""
-
     shell_exec("/bin/kill -%d %d" % (signal, pid))
