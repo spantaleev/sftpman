df5f23b165f06cbb5d03723869009c3e3c78ffe2
Slavi Pantaleev <s.pantaleev@gmail.com>
2011-06-17 19:47:10 +0300

    Removed GUI stuff from launcher. GUI will move to a separate project.

--- sftpman/launcher.py
+++ sftpman/launcher.py
@@ -1,16 +1,5 @@
 #!/usr/bin/python
 
-import sys      
-
-
-def main():
-    if len(sys.argv) == 1:
-        import sftpman.GUI
-        sftpman.GUI.start()
-    else:
-        import sftpman.CLI
-        sftpman.CLI.start()
-    
-
-if __name__ == "__main__":
-    main()
+if __name__ == '__main__':
+    import cli
+    cli.start()
