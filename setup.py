6ad910fae03775057123d84d1221bc41ffe7f4da
Slavi Pantaleev <s.pantaleev@gmail.com>
2012-08-08 12:17:55 +0300

    New release - 0.5.5

--- setup.py
+++ setup.py
@@ -13,7 +13,7 @@ from setuptools import setup
 
 setup(
     name = "sftpman",
-    version = '0.5.4',
+    version = '0.5.5',
     description = "A command-line application that helps you mount SFTP file systems.",
     long_description = __doc__,
     author = "Slavi Pantaleev",
