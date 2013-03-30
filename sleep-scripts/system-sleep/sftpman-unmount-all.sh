a8958a7c79392d514e409d522efa5650178b2b7c
Slavi Pantaleev <s.pantaleev@gmail.com>
2013-03-30 19:09:29 +0200

    Add systemd sleep hook
    systemd does runs its own sleep hooks and does not invoke
    the ones from pm-utils.

---
+++ sleep-scripts/system-sleep/sftpman-unmount-all.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+# Sleep hook for systemd.
+# Takes care of unmounting everything for all users on system suspend/hibernate.
+# To be placed in /usr/lib/systemd/system-sleep/
+
+if [ -n "$1" ]; then
+	if ([ "$1" = "pre" ]); then
+		for username in $(ps aux | grep '/mnt/sshfs/' | grep -v 'grep' | grep -o -E '^(\w+)\s' | uniq); do
+			su -l $username -c 'sftpman unmount_all'
+		done;
+	fi
+fi
+
