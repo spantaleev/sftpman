053476bf75d274e2d6cb381e5d2d8bc1bf6c9c2c
Slavi Pantaleev <s.pantaleev@gmail.com>
2017-07-19 12:15:20 +0300

    Bump version to 0.6.3

--- setup.py
+++ setup.py
@@ -13,7 +13,7 @@ from setuptools import setup
 
 setup(
     name = "sftpman",
-    version = '0.6.2',
+    version = '0.6.3',
     description = "A command-line application that helps you mount SFTP file systems.",
     long_description = __doc__,
     author = "Slavi Pantaleev",
