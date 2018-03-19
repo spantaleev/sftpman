513cdf9bb8b8c278914ae85bdcc698ff4e777caf
Slavi Pantaleev <slavi@devture.com>
2018-03-19 12:08:40 +0200

    Release 1.1.1

--- setup.py
+++ setup.py
@@ -13,7 +13,7 @@ from setuptools import setup
 
 setup(
     name = "sftpman",
-    version = '1.1.0',
+    version = '1.1.1',
     description = "A command-line application that helps you mount SFTP file systems.",
     long_description = __doc__,
     author = "Slavi Pantaleev",
